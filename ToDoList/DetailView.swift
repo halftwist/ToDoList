a112b2c3cec34f51b8f1172e013ec37e9f58cf61
John Kearon <jkearon@hotmail.com>
2025-04-21 20:42:03 -0400

    Swift lesson 63

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,37 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by John Kearon on 4/21/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        VStack {
+           Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            
+            Text("You Are a Swifty Legend!")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Get Back!") {
+                dismiss()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+        .navigationBarBackButtonHidden()
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ ToDoList/ToDoListView.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoListView.swift
+//  ToDoList
+//
+//  Created by John Kearon on 4/21/25.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                NavigationLink {
+                    DetailView()
+                } label: {
+                    Image(systemName: "eye")
+                    Text("Show the New View!")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
+        }
+        
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
