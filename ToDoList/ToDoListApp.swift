e6ed6b73dce8ae9c37ebd1cad25a141ae845357d
John Kearon <jkearon@hotmail.com>
2025-04-21 20:09:18 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by John Kearon on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
